d547d3de2fdbe3f18cdbbab9558c0b21794dcd36
selmanmutlu <selmanmutlu29@gmail.com>
2021-12-30 21:50:20 +0300

    task1-2 yapıldı.

--- task_1.py
+++ task_1.py
@@ -1,15 +1,20 @@
 ##Kullanıcıdan aldığınız 3 tane sayıyı çarparak ekrana yazdırın. Ekrana yazdırma işlemini format metoduyla yapmaya çalışın
+a = int(input("ilk sayıyı giriniz:"))
+b = int(input("ikinci sayıyı giriniz:"))
+c = int(input("üçüncü sayıyı giriniz:"))
 
-
-
+print("ÇÖZÜM: {} x {} x {} = {}".format(a,b,c,a*b*c))
 
 #1) Kullanıcıdan ad,soyad ve numara bilgisini alarak bunları alt alta ekrana yazdırın.
-
-
-
+ad = input("İsminizi Giriniz:")
+soyad = input("Soyisminizi Giriniz:")
+numara = input("Numaranızı Giriniz:")
+print("\n----KULLANICI BİLGİLERİ---- \n İsim: {} \n Soyisim: {} \n Numara: {} ".format(ad,soyad,numara))
 
 #2) Kullanıcıdan bir dik üçgenin dik olan iki kenarını(a,b) alın ve hipotenüs uzunluğunu bulmaya çalışın. Hipotenüs Formülü: a^2 + b^2 = c^2
-
+kenar1 = int(input("Birinci dik kenar değerini giriniz:"))
+kenar2 = int(input("İkinci dik kenar değerini giriniz:"))
+print("Hipotenüs'ün uzunluğu: {} ".format((kenar1**2+kenar2**2)**0.5))
 
 
 """
@@ -23,7 +28,18 @@
 #BKİ 25 ile 30 arasındaysa --------> Fazla Kilolu
 
 #BKİ 30'un üstündeyse -------------> Obez"""
+boy = float(input("Boyunuzu giriniz:"))
+kilo = float(input("Kilonuzu giriniz:"))
+kitle_endeksi = kilo/(boy*boy)
 
+if kitle_endeksi > 30:
+    print("Obez")
+elif kitle_endeksi < 30 and kitle_endeksi > 25:
+    print("Fazla Kilolu")
+elif kitle_endeksi > 18.5 and kitle_endeksi < 25:
+    print("Normal")
+elif kitle_endeksi < 18.5:
+    print("Zayıf")
 
 
 """
@@ -55,12 +71,36 @@ Toplam Not <  55 -----> FF
 
 
 """
+print("""
+********************************************
 
+HARF NOTU UYGULAMASI
 
+********************************************
+""")
+vize1 = int(input("1. Vize Notunu Giriniz:"))
+vize2 = int(input("2. Vize Notunu Giriniz:"))
+final = int(input("Final Notunu Giriniz:"))
+harf_notu = vize1*(3/10) + vize2*(3/10) + final*(4/10)
 
-
-
-
+if harf_notu >= 90:
+    print("AA")
+elif harf_notu >= 85:
+    print("BA")
+elif harf_notu >= 80:
+    print("BB")
+elif harf_notu >= 75:
+    print("CB")
+elif harf_notu >= 70:
+    print("CC")
+elif harf_notu >= 65:
+    print("DC")
+elif harf_notu >= 60:
+    print("DD")
+elif harf_notu >= 55:
+    print("FD")
+else:
+    print("FF")
 
 
 """
@@ -68,11 +108,9 @@ Toplam Not <  55 -----> FF
 1'den 10'kadar olan sayılarla ekrana çarpım tablosu bastırmaya çalışın.
 İpucu: İç içe 2 tane for döngüsü kullanın. Aynı zamanda sayıları range() fonksiyonunu kullanarak elde edin.
 """
-
-
-
-
-
+for i in range(1,10):
+    for j in  range(1,10):
+        print("{} x {} = {}".format(i,j,i*j))
 
 
 
@@ -83,36 +121,60 @@ Her bir while döngüsünde kullanıcıdan bir sayı alın ve kullanıcının gi
 İpucu : while döngüsünü sonsuz koşulla başlatın ve kullanıcı q'ya basarsa döngüyü break ile sonlandırın.
 
 """
+toplam = 0
 
+while True:
+    sayı = input("Sayıyı Giriniz:")
 
+    if sayı == "q":
+        break
+    sayı = int(sayı)
 
-
-
-
-
-
+    toplam += sayı
+print("Toplam:",toplam)
 
 """
 7) 
 Kullanıcıdan aldığınız bir sayının mükemmel olup olmadığını bulmaya çalışın.
 Bir sayının kendi hariç bölenlerinin toplamı kendine eşitse bu sayıya "mükemmel sayı" denir. Örnek olarak, 6 mükemmel bir sayıdır. (1 + 2 + 3 = 6)
 """
+sayı = int(input("sayı:"))
 
+list = []
+x = 1
+toplam = 0
 
+while x < sayı:
+    if sayı % x == 0:
+        list.append(x)
+    x += 1
 
-
-
-
-
+for i in list:
+    toplam += i
+if toplam == sayı:
+    print("girdiğiniz sayı bir mükemmel sayıdır")
+else:
+    print("girdiğiniz sayı bir mükemmel sayı değildir")
 
 """
 8) 
 Kullanıcıdan 2 tane sayı alarak bu sayıların en büyük ortak bölenini (EBOB) dönen bir tane fonksiyon yazın.
 
 """
+def ebobbulma(sayı1,sayı2):
+    i = 1
+    list = []
+    while i <= sayı1 and i <= sayı2:
+        if sayı1 % i == 0 and sayı2 % i == 0:
+            list.append(i)
+        i += 1
 
+    return max(list)
 
+sayı1 = int(input("İlk Sayınızı Giriniz:"))
+sayı2 = int(input("ikinci Sayınızı Giriniz:"))
 
+print("EBOB:",ebobbulma(sayı1,sayı2))
 
 
 """
@@ -120,8 +182,18 @@ Kullanıcıdan 2 tane sayı alarak bu sayıların en büyük ortak bölenini (EB
 Kullanıcıdan 2 tane sayı alarak bu sayıların en küçük ortak katlarını (EKOK) dönen bir tane fonksiyon yazın.
 
 """
+def ekokbulma(sayı1,sayı2):
+    i = 1
+    ekok = 1
+    while True:
+        if sayı1*i % sayı2 == 0:
+            ekok = sayı1*i
+            return ekok
+        i += 1
 
-
+a = int(input("İLK SAYIYI GİRİNİZ:"))
+b = int(input("İKİNCİ SAYIYI GİRİNİZ:"))
+print("EKOK:",ekokbulma(a,b))
 
 """
 10)
@@ -136,3 +208,158 @@ Kuş Sınıfı ------> Bu sınıf, hayvan sınıfından miras alan bir sınıf o
 At Sınıfı ------> Bu sınıf, hayvan sınıfından miras alan bir sınıf olacak. Ayrıca bu sınıfa atlara ait ek özellikler ve metodlar ekleyin.
 
 """
+import  time
+
+class Hayvanlar():
+    def __init__(self,grup="Omurgalılar ve Omurgasızlar",yaşam_alanı="Hava,Kara,Deniz",beslenme_çeşidi="etçil,otçul,hem etçil hem otçul"):
+        self.grup = grup
+        self.yaşam_alanı = yaşam_alanı
+        self.beslenme_çeşidi = beslenme_çeşidi
+    def __str__(self):
+        return ("Hayvanların Ortak Özellikleri \n1)Grup = {}\n2)Yaşam Alanı = {}\n3)Beslenme Çeşidi = {}".format(self.grup,self.yaşam_alanı,self.beslenme_çeşidi))
+
+class Köpekler(Hayvanlar):
+    def __init__(self,grup="Memeliler",yaşam_alanı="Kara",beslenme_çeşidi="Etçil",cins=["Buldog","Golden","Pitbull","Rottweiler"],genel_özellikleri=["Keskin koku alma ve işitme kabiliyetleri vardır.","Ömürleri 15-20 yıl arasıdır.","Sahiplerine bağlıdırlar."]):
+        super().__init__(grup,yaşam_alanı,beslenme_çeşidi)
+        self.cins = cins
+        self.genel_özellikleri = genel_özellikleri
+    def __str__(self):
+        return ("KÖPEKLERİN GENEL ÖZELLİKLERİ \n1)Tür = {}\n2)Yaşam Alanı = {}\n3)Beslenme Çeşidi = {}\n4)Cinsleri = {}\n5)Genel Özellikleri = {}".format(self.grup,self.yaşam_alanı,self.beslenme_çeşidi,self.cins,self.genel_özellikleri))
+    def cins_ekle(self,):
+        a = input("Eklemek istediğiniz cinsi giriniz:")
+        print("Ekleniyor...")
+        time.sleep(2)
+        self.cins.append(a)
+    def özellik_ekleme(self):
+        b = input("Eklemek istediğiniz özelliği giriniz:")
+        print("Ekleniyor...")
+        time.sleep(2)
+        self.genel_özellikleri.append(b)
+
+class Kuşlar(Hayvanlar):
+    def __init__(self,grup="Kuşlar",yaşam_alanı="Hava ve Kara",beslenme_çeşidi="Etçil",cins=["Amazon Papağanı","Muhabbet Kuşu","Hint Bülbülü"],genel_özellikleri=["Vücutları tüylerle kaplıdır.","Yavrularını sütle beslemezler."]):
+        super().__init__(grup,yaşam_alanı,beslenme_çeşidi)
+        self.cins = cins
+        self.genel_özellikleri = genel_özellikleri
+    def __str__(self):
+        return ("KUŞLARIN GENEL ÖZELLİKLERİ \n1)Tür = {}\n2)Yaşam Alanı = {}\n3)Beslenme Çeşidi = {}\n4)Cinsleri = {}\n5)Genel Özellikleri = {}".format(self.grup,self.yaşam_alanı,self.beslenme_çeşidi,self.cins,self.genel_özellikleri))
+    def cins_ekle(self,):
+        a = input("Eklemek istediğiniz cinsi giriniz:")
+        print("Ekleniyor...")
+        time.sleep(2)
+        self.cins.append(a)
+    def özellik_ekleme(self):
+        b = input("Eklemek istediğiniz özelliği giriniz:")
+        print("Ekleniyor...")
+        time.sleep(2)
+        self.genel_özellikleri.append(b)
+
+class Atlar(Hayvanlar):
+    def __init__(self,grup="Memeliler",yaşam_alanı="Kara",beslenme_çeşidi="Otçul",cins=["Arap Atı","Türkmen Atı"],genel_özellikleri=["Kulakları çok iyi işitir.","Bacak kasları çok kuvvetlidir."]):
+        super().__init__(grup,yaşam_alanı,beslenme_çeşidi)
+        self.cins = cins
+        self.genel_özellikleri = genel_özellikleri
+    def __str__(self):
+        return  ("ATLARIN GENEL ÖZELLİKLERİ \n1)Tür = {}\n2)Yaşam Alanı = {}\n3)Beslenme Çeşidi = {}\n4)Cinsleri = {}\n5)Genel Özellikleri = {}".format(self.grup,self.yaşam_alanı,self.beslenme_çeşidi,self.cins,self.genel_özellikleri))
+    def cins_ekle(self,):
+        a = input("Eklemek istediğiniz cinsi giriniz:")
+        print("Ekleniyor...")
+        time.sleep(2)
+        self.cins.append(a)
+    def özellik_ekleme(self):
+        b = input("Eklemek istediğiniz özelliği giriniz:")
+        print("Ekleniyor...")
+        time.sleep(2)
+        self.genel_özellikleri.append(b)
+print("""
+*******************************************************************
+HAYVANLAR ALEMİNİN ORTAK ÖZELLİKLERİ
+
+1) Hayvanların Ortak Özellikleri
+2) Köpeklerin Ortak Özellikleri
+3) Kuşların Ortak Özellikleri
+4) Atların Ortak Özellikleri
+
+(Çıkış yapmak için 'q' ya basınız.)
+********************************************************************
+""")
+while True:
+    a = input("Lütfen göz atmak istediğiniz seçeneği yazınız:")
+    if a == "1":
+        hayvan=Hayvanlar()
+        print(hayvan)
+    elif a == "2":
+       köpek = Köpekler()
+       print(köpek)
+       time.sleep(2)
+       print("""
+       ***************
+       KÖPEKLER:
+       1)Cins Ekle
+       2)Özellik Ekle
+       3)Çıkış
+       ***************
+       """)
+       b = input("Lütfen işlem yapmak istediğiniz numarayı seçiniz:")
+       if b == "1":
+           köpek.cins_ekle()
+       elif b == "2":
+           köpek.özellik_ekleme()
+       elif b == "3":
+           print("Bu menüden çıkış yapılıyor...")
+           time.sleep(1)
+       else:
+           print("Lütfen geçerli bir işlem seçiniz.\nAna menüye dönülüyor...")
+           time.sleep(2)
+    elif a == "3":
+       kuş = Kuşlar()
+       print(kuş)
+       time.sleep(2)
+       print("""
+       ***************
+       KUŞLAR:
+       1)Cins Ekle
+       2)Özellik Ekle
+       3)Çıkış
+       ***************
+       """)
+       b = input("Lütfen işlem yapmak istediğiniz numarayı seçiniz:")
+       if b == "1":
+           kuş.cins_ekle()
+       elif b == "2":
+           kuş.özellik_ekleme()
+       elif b == "3":
+           print("Bu menüden çıkış yapılıyor...")
+           time.sleep(1)
+       else:
+           print("Lütfen geçerli bir işlem seçiniz.\nAna menüye dönülüyor...")
+           time.sleep(2)
+    elif a == "4":
+       at = Atlar()
+       print(at)
+       time.sleep(2)
+       print("""
+       ***************
+       ATLAR:
+       1)Cins Ekle
+       2)Özellik Ekle
+       3)Çıkış
+       ***************
+       """)
+       b = input("Lütfen işlem yapmak istediğiniz numarayı seçiniz:")
+       if b == "1":
+           at.cins_ekle()
+       elif b == "2":
+           at.özellik_ekleme()
+       elif b == "3":
+           print("Bu menüden çıkış yapılıyor...")
+           time.sleep(1)
+       else:
+           print("Lütfen geçerli bir işlem seçiniz.\nAna menüye dönülüyor...")
+           time.sleep(2)
+    elif a == "q":
+        print("Program kapatılıyor...")
+        time.sleep(1)
+        break
+    else:
+        print("Lütfen geçerli bir işlem seçiniz!...")
--- task_2.py
+++ task_2.py
@@ -8,6 +8,7 @@ After a card is dealt, it is removed from the deck.
 -The Card class should have a suit (Hearts, Diamonds, Clubs, Spades) and a value (A,2,3,4,5,6,7,8,9,10,J,Q,K)
 """
 
+
 """
 *2.
 basit bir drone classı yaratın her dronun sahip olması gerek standart özellikler:
@@ -26,16 +27,141 @@ her dronun sahip olması gereken fonksiyonlar(Bu fonksiyonlar hayali bir drone i
 *yük bırak,
 *kalkış noktasına uzaklık,
 """
+import time
+class Drone():
 
+    def __init__(self,id,koordinat,hız,taşıyabiliceği_yük,batarya):
+        self.id = id
+        self.koordinat = koordinat
+        self.hız = hız
+        self.taşıyabiliceği_yük = taşıyabiliceği_yük
+        self.batarya = batarya
+
+    def __str__(self):
+        return "Drone ID: {}\nKoordinatları: {}\nHızı: {}\nTaşıyabiliceği Yük: {}\nBatarya: {}".format(self.id,self.koordinat,self.hız,self.taşıyabiliceği_yük,self.batarya)
+
+    def takeoff(self):
+        a = input("Drone'u havalandırmak istediğiniz y koordinatını giriniz:")
+        b = int(input("Seyahat hızını giriniz:"))
+        print("Drone havalanıyor...")
+        time.sleep(2)
+        self.koordinat["y"] = a
+        self.hız = b
+        print("Yeni koordinatlar {}\nHızı: {}".format(self.koordinat,self.hız))
+
+    def return_to_launch(self):
+        if self.koordinat["x"] == 0 and self.koordinat["y"] == 0 and self.koordinat["z"] == 0:
+            print("Drone zaten başlangıç noktasında!...")
+        else:
+            print("Başlangıç noktasına dönülüyor...")
+            time.sleep(2)
+            self.koordinat["x"] = 0
+            self.koordinat["y"] = 0
+            self.koordinat["z"] = 0
+            print("Drone başlangıç noktasına geri dönüş yaptı.\nKoordinatları = {}".format(self.koordinat))
+
+    def move(self,x,y,z,v):
+        print("Drone istediğiniz koordinatlara ilerliyor...")
+        time.sleep(2)
+        self.koordinat["x"] = x
+        self.koordinat["y"] = y
+        self.koordinat["z"] = z
+        self.hız = v
+        print("Drone ' un son konumu {}\nHızı: {}".format(self.koordinat,self.hız))
+
+    def uzaklık(self):
+        a = (x**2 + y**2 + z**2) ** 0.5
+        a = int(a)
+        print("Drone'un başlangıç noktasına olan uzaklığı {} birim".format(a))
+
+    def yük_al(self):
+        koordinat = input("Alıcağınız yükün koordinatlarını virgün(,) koyarak giriniz:")
+        yük = int(input("Almak istediğiniz ağırlığı giriniz:"))
+        if yük > self.taşıyabiliceği_yük:
+            print("Drone bu ağırlığı kaldıracak kapasitede değil!...")
+        else:
+            print("Drone koordinatlara yönlendiriliyor...")
+            time.sleep(3)
+            liste = koordinat.split(",")
+            self.koordinat["x"] = liste[0]
+            self.koordinat["y"] = liste[1]
+            self.koordinat["z"] = liste[2]
+            print("Drone varış noktasına ulaştı\nDrone konumu: {}\nYük alınıyor..".format(self.koordinat))
+            time.sleep(3)
+            print("Yük başarıyla alındı.")
+
+    def yük_bırak(self):
+        koordinat = input("Yükü bırakmak istediğiniz koordinatları virgün(,) koyarak giriniz:")
+        print("Drone koordinatlara yönlendiriliyor...")
+        time.sleep(3)
+        liste = koordinat.split(",")
+        self.koordinat["x"] = liste[0]
+        self.koordinat["y"] = liste[1]
+        self.koordinat["z"] = liste[2]
+        print("Drone varış noktasına ulaştı\nDrone konumu: {}\nYük bırakılıyor...".format(self.koordinat))
+        time.sleep(3)
+        print("Yük başarıyla bırakıldı.")
+
+AAD_Drone = Drone(id = "412639",koordinat = {"x": 0, "y": 0, "z": 0},hız=0,taşıyabiliceği_yük= 10 ,batarya="%100")
+
+print("""
+*****************************************************
+DRONE KULLANICI PANELİ
+
+1)Drone Bilgileri
+2)Kalkış
+3)Başlangıca Geri Dönüş
+4)Hareket
+5)Yük Al
+6)Yük Bırak
+7)Başlangıç Noktasına Olan Uzaklık
+
+(Lütfen kapatmak için 'q' yu tuşlayın...)
+*****************************************************
+""")
+while True:
+    komut = input("Lütfen komut seçiniz:")
+    if komut == "q":
+        print("Panel kapatılıyor...")
+        time.sleep(2)
+        break
+    elif komut == "1":
+        print(AAD_Drone)
+    elif komut == "2":
+        AAD_Drone.takeoff()
+    elif komut == "3":
+        AAD_Drone.return_to_launch()
+    elif komut == "4":
+        x = int(input("Lütfen varış noktasının x koordinatını giriniz:"))
+        y = int(input("Lütfen varış noktasının y koordinatını giriniz:"))
+        z = int(input("Lütfen varış noktasının z koordinatını giriniz:"))
+        v = int(input("Lütfen seyahat hızını giriniz:"))
+        AAD_Drone.move(x,y,z,v)
+    elif komut == "5":
+        AAD_Drone.yük_al()
+    elif komut == "6":
+        AAD_Drone.yük_bırak()
+    elif komut == "7":
+        AAD_Drone.uzaklık()
+    else:
+        print("Lütfen geçerli bir komut giriniz!...")
 
 """
 *3.
 1000 int den oluşan random bir liste yaratın ve istediğiniz sorting algoritmasını kullanarak küçükten büyüğe sıralayan bir script yazınız.
 """
+import random
 
+liste1 = []
+liste2 = []
+for i in range(0,1000):
+        liste1.append(random.randint(0,2000))
 
-
-
+liste2 = sorted(liste1)
+print(liste1)
+print("******************")
+print(liste2)
+#tam anlayamadım ama sanırım ödev böyle
 
 """
 *4.
@@ -43,7 +169,18 @@ n katlı bir gökdelende olduğunuzu farz edin, elinizde 2 adet yumurta var. ama
 
 n verilen k değerine göre en fazla kaç olabilir?
 """
+"""
+binary search algoritmasıyla yapmayı denedim.
+örnek olarak 3 deneme hakkımız olduğunu varsayalım buna göre n değeri maksimum 8 olabiliyor, ilk denememizi de
+en orta kattan 4. kattan yapıyoruz kırılmassa kalan üst yarının en ortasından yani 6. kattan 2. denemeyi gerçekleştiriyoruz
+bu katta yumurtanın kırıldığını varsayalım bu seferde 4. kat ile 6. katın ortası yani 5. kattan 3. denememizi yapıyoruz,
+yumurtanın kırılıp kırılmamasına göre 3. denememizde sonuca ulaşmış oluyoruz. 
+"""
+deneme_hakkı = int(input("Kaç adet deneme hakkınız olsun? :"))
 
+en_yüksek_kat = 2 ** deneme_hakkı
+
+print("Deneme sayınıza göre maksimum {} katlı bir gökdelende atış yapabilirsiniz.".format(en_yüksek_kat))
 
 
 """
@@ -51,3 +188,14 @@ n verilen k değerine göre en fazla kaç olabilir?
 bir sayının cift olup olmadığını hesaplayan en iyi fonksiyonu yazmaya calışın. (diyer katılımcılarla karşılaştırılacak)
 
 """
+def çiftmi_tekmi(sayı):
+
+    if sayı % 2 == 0:
+        print("{}  çift sayıdır.".format(sayı))
+
+    else:
+        print("{}  tek sayıdır. ".format(sayı))
+
+
+sayı = int(input("Lütfen sayınızı giriniz:"))
+print(çiftmi_tekmi(sayı))
